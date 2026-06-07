44dab23c04dd8e09f1dbb3f73302824d57745d5e
willysaputra_cell <willysaputra@itbss.ac.id>
2026-06-07 22:31:20 +0700

    login dan admin

---
+++ admin/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+include "security.php";
+
+echo "welcome, " . $username;
+?>
+<br>
+<a href="courses/index.php">Manajemen Kelas</a>
+<br>
+<a href="logout.php">logout</a>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php 
+include "header.php";
+?>
+<section>
+    <form action="sv_login.php" method="post">
+        <input type="text" placeholder="username" name="username">
+        <input type="password" placeholder="password" name="password">
+        <button type="submit">login</button>
+    </form>
