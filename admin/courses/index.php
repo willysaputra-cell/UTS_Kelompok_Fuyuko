2238bd56870caa19249e3a8110bb8952f3238bfb
willysaputra_cell <willysaputra@itbss.ac.id>
2026-06-09 21:38:45 +0700

    update index.php dalam product

--- admin/courses/index.php
+++ admin/courses/index.php
@@ -2,7 +2,7 @@
 include "../security.php";
 include "../../koneksi.php";
 
-$sql = "select * from courses";
+$sql = "select * from products";
 $query = mysqli_query($conn, $sql);
 ?>
 <a href="../dashboard.php">Kembali ke Dashboard</a> |
@@ -13,26 +13,28 @@ $query = mysqli_query($conn, $sql);
     <thead>
         <tr>
             <th>No</th>
-            <th>Nama Kelas</th>
-            <th>Deskripsi</th>
+            <th>Name</th>
             <th>Harga</th>
-            <th></th>
+            <th>Foto</th>
+            <th>Id_Kategori</th>
         </tr>
     </thead>
     <tbody>
         <?php
         $no = 1;
         while($result = mysqli_fetch_array($query)) {
-            $photo = $result['photo'];
             $name = $result['name'];
             $price = $result['price'];
+            $image = $result['image'];
+            $category_id = $result['category_id'];
             $id = $result['id'];
         ?>
         <tr>
             <td><?= $no ?></td>
-            <td><?= $photo ?></td>
             <td><?= $name ?></td>
             <td><?= $price ?></td>
+            <td><?= $image ?></td>
+            <td><?= $category_id ?></td>
             <td>
                 <a href="edit.php?id=<?= $id; ?>">Edit</a> |
                 <a href="hapus.php?id=<?= $id; ?>" onclick="return confirm('Yakin ingin menghapus data ini?')">Hapus</a>
