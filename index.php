000246e12136f689883b9f443e97bf3d6048f295
willysaputra_cell <willysaputra@itbss.ac.id>
2026-06-14 23:50:56 +0700

    test

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         </header>
         <section class="pengenalan">
             <div class="kata2">
-                <p>Selamat datang di surga sdfafasf
+                <p>Selamat datang di surga
                 <br>manis! Temukan kue basah, 
                 <br>snack, puding, dan
                 <br>kue kering lezat untuk
