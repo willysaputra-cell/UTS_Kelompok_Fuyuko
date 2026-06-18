1cce2027606d75d2beff46fac1f01602841701ec
willysaputra_cell <willysaputra@itbss.ac.id>
2026-06-18 10:51:46 +0700

    tambah tipis

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         <section class="pengenalan">
             <div class="kata2">
                 <p>Selamat datang di surga
-                <br>manis!! Temukan kue basah, 
+                <br>manis! Temukan kue basah, 
                 <br>snack, puding, dan
                 <br>kue kering lezat untuk
                 <br>setiap momen Anda.</p>
